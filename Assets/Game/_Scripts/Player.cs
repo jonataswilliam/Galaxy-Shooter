2ab81d1e7a8c1b8dc8366fb683bde7757dd7f9cb
Jonatas William <jonatas_2602@hotmail.com>
2018-05-07 22:29:05 -0300

    Add Controles da nave

---
+++ Assets/Game/_Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	[SerializeField] private float speed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector3(0, -3.5f, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+		transform.Translate(Vector3.right * speed * horizontal * Time.deltaTime);
+		transform.Translate(Vector3.up * speed * vertical * Time.deltaTime);
+	}
+}
