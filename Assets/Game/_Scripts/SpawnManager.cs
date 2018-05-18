40565a4aadfc417d4abd2054de79d6b08d2d4337
Jonatas William <jonatas_2602@hotmail.com>
2018-05-18 10:39:35 -0300

    Init Span Manager

--- Assets/Game/_Scripts/SpawnManager.cs
+++ Assets/Game/_Scripts/SpawnManager.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour {
 
+	[SerializeField] private GameObject _enemyShipPrefab;
+	[SerializeField] private GameObject[] _powerUpPrefab;
+
 	// Use this for initialization
 	void Start () {
-		
+		StartCoroutine(SpawnEnemies());
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	IEnumerator SpawnEnemies () {
+		while(true) {
+			float randomX = Random.Range(-8.0f, 8.0f);
+			Vector3 spawnPosition = new Vector3(randomX, 6.5f, 0f);
+
+			Instantiate(_enemyShipPrefab, spawnPosition, Quaternion.identity);
+			yield return new WaitForSeconds(1.0f);
+		}
+
 	}
 }
